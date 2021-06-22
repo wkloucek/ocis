8450ae1fafee21778f08d7c64d531c3ea13f4d8a
Jörn Friedrich Dreyer <jfd@butonic.de>
2021-06-22 13:59:02 +0000

    add more containers
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>

---
+++ model/README.md
@@ -0,0 +1,24 @@
+# C4 model
+
+We use a c4 model to keep all diagrams consistent. This directory contains the model and generated diagrams.
+
+See https://github.com/goadesign/model for details on the tooling we use.
+
+# Install
+
+```console
+# GO111MODULE=off go get goa.design/model/cmd/mdl
+```
+_TODO add `mdl` to bingo_
+
+# Layouting the graphs
+
+In this directory run
+
+```console
+# mdl serve goa.design/model/examples/basic/model
+Watching: /home/vscode/repositories/ocis/model
+Editor started. Open http://localhost:8080 in your browser.
+```
+
+
--- model/model.go
+++ model/model.go
@@ -14,7 +14,7 @@ var _ = Design(func() {
 
 		Tag("Person")
 	})
-	var EndUser = Person("End User", "A person part of an enterprise", func() {
+	/*var EndUser =*/ Person("End User", "A person part of an enterprise", func() {
 		External()
 
 		Uses("oCIS", "manages files, folders and shares with", Synchronous, func() {
@@ -60,14 +60,32 @@ var _ = Design(func() {
 
 	var (
 		// Forward declaration so variables can be used to define views.
-		OcisProxy   *expr.Container
-		OcisStorage *expr.Container
-		OcisWeb     *expr.Container
+
+		// Containers aka every deployable thing
+
 		// clients
 		WebSinglePageApp *expr.Container
-		AndroidApp       *expr.Container
-		IOSApp           *expr.Container
-		DesktopClient    *expr.Container
+		//AndroidApp       *expr.Container
+		//IOSApp           *expr.Container
+		//DesktopClient    *expr.Container
+
+		OcisProxy *expr.Container
+		//OcisWeb        *expr.Container
+		LibreGraph     *expr.Container
+		OcisThumbnails *expr.Container
+
+		// frontend
+		RevaOCDav *expr.Container
+		RevaOCS   *expr.Container
+
+		// reva
+		RevaGateway              *expr.Container
+		RevaStorageRegistry      *expr.Container
+		RevaHomeStorageProvider  *expr.Container
+		RevaUsersStorageProvider *expr.Container
+		//RevaPublicStorageProvider *expr.Container
+		// Components aka things inside Containers
+
 	)
 
 	var oCISSystem = SoftwareSystem("oCIS", "Enterprise File Sync and Share", func() {
@@ -77,167 +95,154 @@ var _ = Design(func() {
 
 		Tag("Software System")
 
-		// TODO hide this it only clutters the container diagram, similar to a message bus
-		// TODO all clients "Makes API calls to" ... the proxy. better to show actual calls to the graph, frontend ...
-		// TODO indicate that there is an API gateway that can route based on user ... where? in the label of a relationship?
-		OcisProxy = Container("ocis proxy", "routes requests based on the logged in user", "golang, go-micro", func() {
+		/*AndroidApp =*/
+		Container("Android App", "Provides a limited subset of the Internet banking functionality to customers via their mobile device.", "kotlin", func() {
 			Uses(IdentityManagementSystem, "Makes API calls to", "OpenId Connect", Synchronous, func() {
 				Tag("Relationship", "Synchronous")
 			})
-			Uses(OcisStorage, "forwards API calls to", "HTTP", Synchronous, func() {
-				Tag("Relationship", "Synchronous")
-			})
-			Tag("Element", "Container")
-		})
-
-		OcisStorage = Container("ocis storage", "an opinionated reva configuration", "golang, reva", func() {
-			Component("frontend", "runs all web facing services", "golang, reva", func() {
-				Uses("gateway", "Uses", "CS3/GRPC", Synchronous, func() {
-					Tag("Relationship", "Synchronous")
-				})
-				Tag("Element", "Component")
-			})
-
-			// TODO leave out the gateway?
-			Component("gateway", "api gateway for reva", "reva", func() {
-				Uses("Identity Management", "Uses", Synchronous, func() {
-					Tag("Relationship", "Synchronous")
-				})
-				Tag("Element", "Component")
-			})
-			Component("storage provider", "implements access to a storage system", "reva", func() {
-				Uses(StorageSystem, "reads and writes to", Synchronous, func() {
-					Tag("Relationship", "Synchronous")
-				})
-				Tag("Element", "Component")
-			})
-			Uses(StorageSystem, "reads and writes from", "POSIX, SMB, S3", Synchronous, func() {
-				Tag("Relationship", "Synchronous")
-			})
-		})
-
-		AndroidApp = Container("Android App", "Provides a limited subset of the Internet banking functionality to customers via their mobile device.", "kotlin", func() {
-			Uses(OcisProxy, "Makes API calls to", "WebDAV, OCS", Synchronous, func() {
-				Tag("Relationship", "Synchronous")
-			})
-			Uses("ocis storage/frontend", "Makes API calls to", "JSON/HTTPS", Synchronous, func() {
+			Uses(OcisProxy, "syncs with", "WebDAV, OCS, LibreGraph", Synchronous, func() {
 				Tag("Relationship", "Synchronous")
 			})
 			Tag("Element", "Container", "Mobile App")
 		})
 
-		IOSApp = Container("iOS App", "Provides a limited subset of the Internet banking functionality to customers via their mobile device.", "xcode", func() {
-			Uses(OcisProxy, "Makes API calls to", "WebDAV, OCS", Synchronous, func() {
+		/*IOSApp =*/
+		Container("iOS App", "Provides a limited subset of the Internet banking functionality to customers via their mobile device.", "xcode", func() {
+			Uses(IdentityManagementSystem, "Makes API calls to", "OpenId Connect", Synchronous, func() {
 				Tag("Relationship", "Synchronous")
 			})
-			Uses("ocis storage/frontend", "Makes API calls to", "JSON/HTTPS", Synchronous, func() {
+			Uses(OcisProxy, "syncs with", "WebDAV, OCS, LibreGraph", Synchronous, func() {
 				Tag("Relationship", "Synchronous")
 			})
 			Tag("Element", "Container", "Mobile App")
 		})
 
 		WebSinglePageApp = Container("Web Single-Page Application", "Provides all of the Internet banking functionality to customers via their web browser.", "JavaScript and Angular", func() {
-			Uses("ocis storage/frontend", "Submits credentials to", "JSON/HTTPS", Synchronous, func() {
+			Uses(IdentityManagementSystem, "Makes API calls to", "OpenId Connect", Synchronous, func() {
 				Tag("Relationship", "Synchronous")
 			})
-			Uses(OcisProxy, "Makes API calls to", "WebDAV, OCS", Synchronous, func() {
+			Uses(OcisProxy, "syncs with", "WebDAV, OCS, LibreGraph", Synchronous, func() {
 				Tag("Relationship", "Synchronous")
 			})
 			Tag("Element", "Container", "Web Browser")
 		})
 
-		OcisWeb = Container("ocis web", "Delivers the static content and the ocis web single page application.", "golang and vue", func() {
+		/*OcisWeb =*/
+		Container("ocis web", "Delivers the static content and the ocis web single page application.", "golang and vue", func() {
 			Uses(WebSinglePageApp, "Delivers to the users web browser", Synchronous, func() {
 				Tag("Relationship", "Synchronous")
 			})
 			Tag("Element", "Container")
 		})
 
-		DesktopClient = Container("Desktop client", "syncs files with the users computer", "C++", func() {
-			Uses(OcisProxy, "syncs with", "WebDAV, OCS, openGraphAPI", Synchronous, func() {
+		/*DesktopClient =*/
+		Container("Desktop client", "syncs files with the users computer", "C++", func() {
+			Uses(IdentityManagementSystem, "Makes API calls to", "OpenId Connect", Synchronous, func() {
+				Tag("Relationship", "Synchronous")
+			})
+			Uses(OcisProxy, "syncs with", "WebDAV, OCS, LibreGraph", Synchronous, func() {
 				Tag("Relationship", "Synchronous")
 			})
 			Tag("Element", "Container", "Desktop Client")
 		})
-	})
 
-	var (
-		// Forward declarations so variables can be used in deployment views.
-		DataCenter         *expr.DeploymentNode
-		BigBankAPI         *expr.DeploymentNode
-		LiveAPIApp         *expr.DeploymentNode
-		LiveAPIAppInstance *expr.ContainerInstance
-		BigBankWeb         *expr.DeploymentNode
-		LiveWebApp         *expr.DeploymentNode
-		LiveWebAppInstance *expr.ContainerInstance
-		CustomerComputer   *expr.DeploymentNode
-		LiveWebBrowser     *expr.DeploymentNode
-		CustomerSPA        *expr.ContainerInstance
-		CustomerMobile     *expr.DeploymentNode
-		CustomerAndroidApp *expr.ContainerInstance
-	)
+		// TODO hide this it only clutters the container diagram, similar to a message bus
+		// TODO all clients "Makes API calls to" ... the proxy. better to show actual calls to the graph, frontend ...
+		// TODO indicate that there is an API gateway that can route based on user ... where? in the label of a relationship?
 
-	DeploymentEnvironment("Live", func() {
-
-		DataCenter = DeploymentNode("Enterprise", "", "Enterprise data center", func() {
-			Tag("Element", "Deployment Node")
-
-			BigBankAPI = DeploymentNode("bigbank-api***", "A web server residing in the web server farm, accessed via F5 BIG-IP LTMs.", "Ubuntu 16.04 LTS", func() {
-				Tag("Element", "Deployment Node")
-				Instances(8)
-				Prop("Location", "London and Reading")
-
-				LiveAPIApp = DeploymentNode("Apache Tomcat", "An open source Java EE web server.", "Apache Tomcat 8.x", func() {
-					Tag("Element", "Deployment Node")
-					Prop("Java Version", "8")
-					Prop("Xms", "1024M")
-					Prop("Xmx", "512M")
-
-					LiveAPIAppInstance = ContainerInstance(OcisProxy, func() {
-						Tag("Container Instance")
-						InstanceID(2)
-					})
-				})
+		OcisProxy = Container("ocis proxy", "routes requests based on the logged in user", "golang, go-micro", func() {
+			Uses(IdentityManagementSystem, "forwards and makes API calls to", "OpenId Connect", Synchronous, func() {
+				Tag("Relationship", "Synchronous")
 			})
-
-			BigBankWeb = DeploymentNode("bigbank-web***", "A web server residing in the web server farm, accessed via F5 BIG-IP LTMs.", "Ubuntu 16.04 LTS", func() {
-				Tag("Element", "Deployment Node")
-				Instances(4)
-				Prop("Location", "London and Reading")
-
-				LiveWebApp = DeploymentNode("Apache Tomcat", "An open source Java EE web server.", "Apache Tomcat 8.x", func() {
-					Tag("Element", "Deployment Node")
-					Prop("Java Version", "8")
-					Prop("Xms", "1024M")
-					Prop("Xmx", "512M")
-
-					LiveWebAppInstance = ContainerInstance(OcisWeb, func() {
-						Tag("Container Instance")
-						InstanceID(2)
-					})
-				})
+			Uses(RevaOCDav, "forwards /(web)dav WebDAV API calls to", "HTTP", Synchronous, func() {
+				Tag("Relationship", "Synchronous")
 			})
+			Uses(RevaOCS, "forwards /ocs API calls to", "HTTP", Synchronous, func() {
+				Tag("Relationship", "Synchronous")
+			})
+			Uses(LibreGraph, "forwards /graph API calls to", "HTTP", Synchronous, func() {
+				Tag("Relationship", "Synchronous")
+			})
+			Uses(OcisThumbnails, "forwards thumbnail preview API calls to", "HTTP", Synchronous, func() {
+				Tag("Relationship", "Synchronous")
+			})
+			// TODO OCM
+			Tag("Element", "Container")
 		})
 
-		CustomerComputer = DeploymentNode("Customer's computer", "", "Microsoft Windows or Apple macOS", func() {
-			Tag("Element", "Deployment Node")
+		LibreGraph = Container("graph", "implements libregraph for /me/drives and more", "golang, reva", func() {
+			Uses(RevaGateway, "Uses", "CS3/GRPC", Synchronous, func() {
+				Tag("Relationship", "Synchronous")
+			})
+			Tag("Element", "Component")
+		})
+		OcisThumbnails = Container("ocis thumbnails", "generates thumbnails", "golang, go-micro", func() {
+			Uses(RevaOCDav, "fetch public thumbnails", "HTTP", Synchronous, func() {
+				Tag("Relationship", "Synchronous")
+			})
+			Uses(RevaGateway, "fetch privats thumbnails", "CS3/GRPC", Synchronous, func() {
+				Tag("Relationship", "Synchronous")
+			})
+			Tag("Element", "Container")
+		})
+		RevaOCDav = Container("ocdav", "implements ownCloud flavoured WebDAV", "golang, reva", func() {
+			Uses(RevaGateway, "Uses", "CS3/GRPC", Synchronous, func() {
+				Tag("Relationship", "Synchronous")
+			})
+			Tag("Element", "Component")
+		})
+		RevaOCS = Container("ocs", "implements openCollaborationServices for sharing and user provisioning", "golang, reva", func() {
+			Uses(RevaGateway, "Uses", "CS3/GRPC", Synchronous, func() {
+				Tag("Relationship", "Synchronous")
+			})
+			Tag("Element", "Component")
+		})
+		RevaGateway = Container("reva gateway", "the gateway to all reva services", "golang, reva", func() {
+			Uses(RevaStorageRegistry, "looks up storage provider address and port", "POSIX, SMB, S3", Synchronous, func() {
+				Tag("Relationship", "Synchronous")
+			})
+			Uses(RevaHomeStorageProvider, "forwards cs3:///home requests", "POSIX, SMB, S3", Synchronous, func() {
+				Tag("Relationship", "Synchronous")
+			})
+			Uses(RevaUsersStorageProvider, "forwards cs3:///users requests", "POSIX, SMB, S3", Synchronous, func() {
+				Tag("Relationship", "Synchronous")
+			})
+			Tag("Element", "Container")
 
-			LiveWebBrowser = DeploymentNode("Web Browser", "", "Chrome, Firefox, Safari or Edge", func() {
-				Tag("Element", "Deployment Node")
+		})
 
-				CustomerSPA = ContainerInstance(WebSinglePageApp, func() {
-					Tag("Container Instance")
+		RevaStorageRegistry = Container("reva storage registry", "api gateway for reva", "golang, reva", func() {
+			Tag("Element", "Container")
+		})
+		RevaHomeStorageProvider = Container("reva home storage provider", "api gateway for reva", "golang, reva", func() {
+			Uses(StorageSystem, "reads and writes to", Synchronous, func() {
+				Tag("Relationship", "Synchronous")
+			})
+			Component("storage driver", "a storage driver", "Go storage.FS interface", func() {
+				Uses(StorageSystem, "reads and writes to", Synchronous, func() {
+					Tag("Relationship", "Synchronous")
 				})
 			})
+			Tag("Element", "Container")
 		})
-
-		CustomerMobile = DeploymentNode("Customer's mobile device", "", "Apple iOS or Android", func() {
-			Tag("Element", "Deployment Node")
-
-			CustomerAndroidApp = ContainerInstance(AndroidApp, func() {
-				Tag("Container Instance")
+		RevaUsersStorageProvider = Container("reva users storage provider", "api gateway for reva", "golang, reva", func() {
+			Uses(StorageSystem, "reads and writes to", Synchronous, func() {
+				Tag("Relationship", "Synchronous")
 			})
+			Component("storage driver", "a storage driver", "Go storage.FS interface", func() {
+				Uses(StorageSystem, "reads and writes to", Synchronous, func() {
+					Tag("Relationship", "Synchronous")
+				})
+			})
+			Tag("Element", "Container")
 		})
+		/*RevaPublicStorageProvider =*/ Container("reva public storage provider", "api gateway for reva", "golang, reva", func() {
+			Uses(RevaGateway, "reads and writes to", Synchronous, func() {
+				Tag("Relationship", "Synchronous")
+			})
+			Tag("Element", "Container")
+		})
+
 	})
 
 	Views(func() {
@@ -262,38 +267,7 @@ var _ = Design(func() {
 
 		ContainerView(oCISSystem, "oCIS Containers", "The container diagram for the oCIS System.", func() {
 			PaperSize(SizeA5Landscape)
-
-			Add(EndUser, func() {
-				Coord(1056, 24)
-			})
-			Add(IdentityManagementSystem, func() {
-				Coord(2012, 1214)
-			})
-			Add(StorageSystem, func() {
-				Coord(2012, 1214)
-			})
-			Add(WebSinglePageApp, func() {
-				Coord(780, 664)
-			})
-			Add(AndroidApp, func() {
-				Coord(1283, 664)
-			})
-			Add(IOSApp, func() {
-				Coord(1283, 664)
-			})
-			Add(OcisWeb, func() {
-				Coord(37, 664)
-			})
-			Add(DesktopClient, func() {
-				Coord(1283, 664)
-			})
-			Add(OcisProxy, func() {
-				Coord(1031, 1214)
-			})
-			Add(OcisStorage, func() {
-				Coord(1031, 1214)
-			})
-
+			AddContainers()
 			// Make software system boundaries visible for "external" containers
 			// (those outside the software system in scope).
 			SystemBoundariesVisible()
@@ -301,23 +275,6 @@ var _ = Design(func() {
 			AutoLayout(RankTopBottom)
 		})
 
-		DeploymentView(oCISSystem, "Live", "LiveDeployment", "An example live deployment scenario for the oCIS System.", func() {
-			PaperSize(SizeA5Landscape)
-			AutoLayout(RankLeftRight)
-
-			Add(LiveWebBrowser)
-			Add(CustomerComputer)
-			Add(CustomerMobile)
-			Add(DataCenter)
-			Add(BigBankWeb)
-			Add(LiveWebApp)
-			Add(BigBankAPI)
-			Add(LiveAPIApp)
-
-			AnimationStep(LiveWebBrowser, CustomerSPA, CustomerComputer)
-			AnimationStep(CustomerMobile, CustomerAndroidApp)
-			AnimationStep(DataCenter, BigBankWeb, LiveWebApp, LiveWebAppInstance, BigBankAPI, LiveAPIApp, LiveAPIAppInstance)
-		})
 		Styles(func() {
 			ElementStyle("Software System", func() {
 				Background("#1168bd")
